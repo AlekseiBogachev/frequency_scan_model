cce1b4cff4350402b577701b561a1ed4db66ca7f
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2022-04-09 11:38:03 +0300

    monoexponential model with tensorflow
    First version of model is created and tested

---
+++ model.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+def print_results(frequency, actual_dlts, initial_model, history = None, final_model=None):
+    print('Initial values:')
+    print(f'Time constant = {initial_model.time_constant.numpy():.4f} s')
+    print(f'Amplitude = {initial_model.amplitude.numpy():.4f} pF')
+    init_mse = tf.keras.metrics.mean_squared_error(actual_dlts, initial_model(frequency))
+    print(f'MSE = {init_mse:.4f}')
+    print(f'RMSE = {tf.sqrt(init_mse):.6f}')
+    
+    if not (final_model is None):
+        print('\nFinal values:')
+        print(f'Time constant = {final_model.time_constant.numpy():.4f} s')
+        print(f'Amplitude = {final_model.amplitude.numpy():.4f} pf')
+        final_mse = tf.keras.metrics.mean_squared_error(actual_dlts, final_model(frequency))
+        print(f'MSE = {final_mse:.4f}')
+        print(f'RMSE = {tf.sqrt(final_mse):.6f}')
+        
+
+    plt.semilogx(frequency, actual_dlts, '.g', label="Actual values", alpha=0.3)
+    plt.semilogx(frequency, initial_model(frequency), '-.b', label="Initial model", alpha=0.5)
+    if not (final_model is None):
+        plt.semilogx(frequency, final_model(frequency), 'r', label="Final model")
+    plt.legend()
+    plt.show()
+
+    if not ((final_model is None) or (history is None)):
+        plt.plot(history.history['loss'])
+        plt.xlabel('Epoch')
+        plt.ylim([0, max(plt.ylim())])
+        plt.ylabel('Loss [Mean Squared Error]')
+        plt.title('Keras training progress')
+        plt.show()
