b754d704a6ee6a4c8d0569d3f62aef11d1263351
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2022-04-09 12:33:11 +0300

    Model improvements
    В модели в качестве параметра вместо постоянной времени выбрана степень постоянной времени, что улучшило сходимость, а также устранило проблему отрицательного значения постоянной времени.

--- model.py
+++ model.py
@@ -4,16 +4,26 @@ import numpy as np
 
 def print_results(frequency, actual_dlts, initial_model, history = None, final_model=None):
     print('Initial values:')
-    print(f'Time constant = {initial_model.time_constant.numpy():.4f} s')
+    
+    time_constant_power = initial_model.time_constant_power.numpy()
+    print(f'Time constant power = {time_constant_power:.4f} log10(s)')
+    print(f'Time constant = {10**time_constant_power:.4f} s')
+    
     print(f'Amplitude = {initial_model.amplitude.numpy():.4f} pF')
+    
     init_mse = tf.keras.metrics.mean_squared_error(actual_dlts, initial_model(frequency))
     print(f'MSE = {init_mse:.4f}')
     print(f'RMSE = {tf.sqrt(init_mse):.6f}')
     
     if not (final_model is None):
         print('\nFinal values:')
-        print(f'Time constant = {final_model.time_constant.numpy():.4f} s')
+        
+        time_constant_power = final_model.time_constant_power.numpy()
+        print(f'Time constant power = {time_constant_power:.4f} log10(s)')
+        print(f'Time constant = {10**time_constant_power:.4f} s')
+        
         print(f'Amplitude = {final_model.amplitude.numpy():.4f} pf')
+        
         final_mse = tf.keras.metrics.mean_squared_error(actual_dlts, final_model(frequency))
         print(f'MSE = {final_mse:.4f}')
         print(f'RMSE = {tf.sqrt(final_mse):.6f}')
