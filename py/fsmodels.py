f47b3f361614a2967bb57e7915f7dcd7a6a11506
AlekseiBogachev <bogachev-al2012@yandex.ru>
2022-07-29 16:53:28 +0300

    Рефакторинг. Продолжение

--- py/fsmodels.py
+++ py/fsmodels.py
@@ -1,6 +1,11 @@
 """Модуль, содержащий модели частотных сканов, полученных на DLS-82E.
 """
 
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
 class FrequencyScan(tf.Module):
     """Модель моноэкспоненциального частотного скана с показателем p.
 
@@ -11,7 +16,110 @@ class FrequencyScan(tf.Module):
     Термин "моноэкспоненциальный" подразумевает, что модель имеет только
     один набор параметров сигнала релаксации (амплитуда и частота).
     """
+
+
+    @property
+    def amplitude(self):
+        if self._tf_in_out:
+            return self._amplitude
+        else:
+            return self._amplitude.numpy()
+
+    @amplitude.setter
+    def amplitude(self, val):
+        self._amplitude = tf.Variable(val, dtype='float64')
+
+
+    @property
+    def time_constant_power(self):
+        if self._tf_in_out:
+            return self._time_constant_power
+        else:
+            return self._time_constant_power.numpy()
+
+    @time_constant_power.setter
+    def time_constant_power(self, val):
+        self._time_constant_power = tf.Variable(val, dtype='float64')
+
+
+    @property
+    def filling_pulse(self):
+        if self._tf_in_out:
+            return self._filling_pulse
+        else:
+            return self._filling_pulse.numpy()
+
+    @filling_pulse.setter
+    def filling_pulse(self, val):
+        self._filling_pulse = tf.Variable(val, dtype='float64')
+
+
+    @property
+    def p_coef(self):
+        if self._tf_in_out:
+            return self._p_coef
+        else:
+            return self._p_coef.numpy()
+
+    @p_coef.setter
+    def p_coef(self, val):
+        self._p_coef = tf.Variable(val, dtype='float64')
+
+
+    @property
+    def fit_p_coef(self):
+        return self._fit_p_coef
+
+    @fit_p_coef.setter
+    def fit_p_coef(self, val):
+        self._fit_p_coef = val
+
+
+    @property
+    def learning_rate(self):
+        return self._learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, val):
+        self._learning_rate = val
     
+
+    @property
+    def n_iters(self):
+        return self._n_iters
+
+    @n_iters.setter
+    def n_iters(self, val):
+        self._n_iters = val
+
+
+    @property
+    def stop_val(self):
+        return self._stop_val
+
+    @stop_val.setter
+    def stop_val(self, val):
+        self._stop_val = val
+
+
+    @property
+    def verbose(self):
+        return self._verbose
+
+    @verbose.setter
+    def verbose(self, val):
+        self._verbose = val
+
+        
+    @property
+    def tf_in_out(self):
+        return self._tf_in_out
+
+    @tf_in_out.setter
+    def tf_in_out(self, val):
+        self._tf_in_out = val
+    
+
     def __init__(self,
                  amplitude = 3.5,
                  time_constant_power = -2.0,
@@ -29,84 +137,19 @@ class FrequencyScan(tf.Module):
                  **kwargs
                 ):
         super().__init__(**kwargs)
-        
-        self._amplitude = tf.Variable(amplitude, dtype='float64')
-        self._time_constant_power = tf.Variable(time_constant_power, 
-                                               dtype='float64')
-        self._filling_pulse = tf.Variable(filling_pulse, dtype='float64')
-        self._p_coef = tf.Variable(p_coef, dtype='float64')
+
+        self.tf_in_out = tf_in_out
+
+        self.amplitude = amplitude
+        self.time_constant_power = time_constant_power
+        self.filling_pulse = filling_pulse
+        self.p_coef = p_coef
         
         self.fit_p_coef = fit_p_coef
         self.learning_rate = learning_rate
         self.n_iters = n_iters
         self.stop_val = stop_val
         self.verbose = verbose
-
-        self.tf_in_out = tf_in_out
-
-
-    @property
-    def amplitude(self):
-        if self.tf_in_out:
-            return self._amplitude
-        else:
-            return self._amplitude.numpy()
-
-
-    @amplitude.setter
-    def amplitude(self, a):
-        if self.tf_in_out:
-            self._amplitude = a
-        else:
-            self._amplitude = tf.Variable(a, dtype='float64')
-
-
-    @property
-    def time_constant_power(self):
-        if self.tf_in_out:
-            return self._time_constant_power
-        else:
-            return self._time_constant_power.numpy()
-
-
-    @time_constant_power.setter
-    def time_constant_power(self, t):
-        if self.tf_in_out:
-            self._time_constant_power = t
-        else:
-            self._time_constant_power = tf.Variable(t, dtype='float64')
-
-
-    @property
-    def filling_pulse(self):
-        if self.tf_in_out:
-            return self._filling_pulse
-        else:
-            return self._filling_pulse.numpy()
-
-
-    @filling_pulse.setter
-    def filling_pulse(self, f):
-        if self.tf_in_out:
-            self._filling_pulse = f 
-        else:
-            self._filling_pulse = tf.Variable(f, dtype='float64')
-
-
-    @property
-    def p_coef(self):
-        if self.tf_in_out:
-            return self._p_coef
-        else:
-            return self._p_coef.numpy()
-
-
-    @p_coef.setter
-    def p_coef(self, p):
-        if self.tf_in_out:
-            self._p_coef = p
-        else:
-            self._p_coef = tf.Variable(p, dtype='float64')
 
 
     def _get_phi(self,
@@ -153,7 +196,9 @@ class FrequencyScan(tf.Module):
         return 1 / self._get_phi(max_freq_pow)
         
         
-    def __call__(self, frequency_powers):
+    def __call__(self, f_powers):
+
+        frequency_powers = tf.Variable(f_powers, dtype='float64')
         
         M = self._get_M(learning_rate=0.2,
                        n_iters=100,
@@ -165,9 +210,13 @@ class FrequencyScan(tf.Module):
     
     
     def fit(self,
-            frequency_powers,
-            dlts,
+            f_powers,
+            dlts_vals,
            ):
+
+        frequency_powers = tf.Variable(f_powers, dtype='float64')
+        dlts = tf.Variable(dlts_vals, dtype='float64')
+
         
         prev_loss = tf.Variable(np.inf, dtype='float64')
         
@@ -176,12 +225,12 @@ class FrequencyScan(tf.Module):
                                             'p_coef', 
                                             'loss'])
         
-        for _ in range(self.n_iters):
+        for _ in range(self._n_iters):
             with tf.GradientTape() as tape:
                 predicted_dlts = self.__call__(frequency_powers)
                 current_loss = tf.reduce_mean(tf.square(dlts - predicted_dlts))
                 
-            if self.fit_p_coef:
+            if self._fit_p_coef:
                 dampl, dtime_const_pow, dp_coef = tape.gradient(current_loss,
                     [self._amplitude, self._time_constant_power, self._p_coef])
             else:
@@ -193,20 +242,23 @@ class FrequencyScan(tf.Module):
             fit_results.loc[_, 'p_coef'] = self._p_coef.numpy()
             fit_results.loc[_, 'loss'] = current_loss.numpy()     
             
-            if self.verbose:
+            if self._verbose:
                 print('iter #', _)
-                print('amp:',self._amplitude)
-                print('tau:',self._time_constant_power)
-                print('p:', self._p_coef)
-                print('Loss:', current_loss)
+                print('amp:',self.amplitude)
+                print('tau:',self.time_constant_power)
+                print('p:', self.p_coef)
+                if self._tf_in_out:
+                    print('Loss:', current_loss)
+                else:
+                    print('Loss:', current_loss.numpy())
                 
-            self._amplitude.assign_sub(self.learning_rate * dampl)
-            self._time_constant_power.assign_sub(self.learning_rate * dtime_const_pow)
-            if self.fit_p_coef:
-                self._p_coef.assign_sub(self.learning_rate * dp_coef)
+            self._amplitude.assign_sub(self._learning_rate * dampl)
+            self._time_constant_power.assign_sub(self._learning_rate * dtime_const_pow)
+            if self._fit_p_coef:
+                self._p_coef.assign_sub(self._learning_rate * dp_coef)
                 
-            if self.stop_val is not None:
-                if tf.abs(current_loss - prev_loss) < self.stop_val:
+            if self._stop_val is not None:
+                if tf.abs(current_loss - prev_loss) < self._stop_val:
                     break
                     
             prev_loss = current_loss
